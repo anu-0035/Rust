8dfb6f0634bcae214e7e73ac3edfefbda6acee31
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2025-05-16 14:47:46 +0530

    Create fun.rs

---
+++ fun.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let num1 = 8;
+    let num2 = 10;
+    let res = add(num1,num2);
+    println!("The sum num1 and num2 = {res}");
+    // print_val(5);
+
+
+
+}
+
+fn add(n1:u32,n2:u32)-> u32{
+    return n1 + n2;
+}
+// fn print_val(item:u8){
+//     println!("{item}");
+// }
